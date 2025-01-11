51079c27714b0a6fe359057f6e5a41b63c394128
khoidinh-teq <khoidinh@teqnological.asia>
2025-01-11 15:55:04 +0700

    feat: config color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,30 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        white: "#FFFFFF",
+        black: {
+          100: "#000000",
+          200: "#1C1B1F",
+          300: "#79747E",
+        },
+        primary: {
+          100: "#A5EBD3",
+          200: "#9BE0C8",
+          300: "#8DD3BB",
+          400: "#82CBB2",
+        },
+        gray: {
+          100: "#D2D1D3",
+          200: "#8F8C91",
+        },
+        error: {
+          100: "#FD736E",
+        },
+      },
+    },
   },
   plugins: [],
 }
